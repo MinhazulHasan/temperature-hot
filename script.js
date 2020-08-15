c5373bb89493e8bdc14585d4b318135c0e1aea0d
MinhazulHasan <minhazul.hasan.sohan@gmail.com>
2020-08-15 21:54:40 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const api = {
-    key: "fbf9967b7898810d1c2d9bec7fed35c3",
-    base: "https://api.openweathermap.org/data/2.5/"
-}
+const key = "fbf9967b7898810d1c2d9bec7fed35c3";
+
 const place = document.getElementById('place');
 const temperature = document.getElementById('temperature');
 const conditionImage = document.getElementById('image');
 const condition = document.getElementById('condition');
 
-document.getElementById('submit-btn').addEventListener('click', getResult);
+document.getElementById('submit-btn').addEventListener('click', displayResults);
 
-function getResult() {
+function displayResults() {
     const city = document.getElementById('keyword').value;
     if (city === '') {
         place.innerText = 'Please Enter Your Location First 😕';
+        clearField();
     }
     else {
-        fetchAPI(city);
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`)
+            .then(res => res.json())
+            .then(data => {
+                temperature.innerHTML = `${data.main.temp.toFixed(0)}&deg;C`;
+                place.innerText = data.name;
+                condition.innerText = data.weather[0].main;
+                conditionImage.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+                document.getElementById('keyword').value = '';
+            })
+            .catch(()=>{
+                place.innerText = "Sorry!!! Location Not Found 😒";
+                clearField();
+            })
     }
 }
 
-function fetchAPI(query) {
-    fetch(`${api.base}weather?q=${query}&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(displayResults);
-}
-
-function displayResults(loc) {
-    // console.log(loc);
-    if (loc.cod == '404') {
-        place.innerText = "Sorry!!! Location Not Found 😒";
-        document.getElementById('keyword').value = '';
-    }
-    else {
-        place.innerText = loc.name;
-        temperature.innerHTML = `${(loc.main.temp - 273.15).toFixed(2)}&deg;C`;
-        conditionImage.src = `https://openweathermap.org/img/wn/${loc.weather[0].icon}@2x.png`;
-        condition.innerText = loc.weather[0].main;
-        document.getElementById('keyword').value = '';
-    }
+function clearField() {
+    temperature.innerText = '';
+    conditionImage.src = '#';
+    condition.innerText = '';
+    document.getElementById('keyword').value = '';
 }
